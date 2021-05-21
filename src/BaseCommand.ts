4da47e252bb838e8ad4fd1fb580805e6fe8a23d7
hillaliy <hillaliy@me.com>
2021-05-21 22:20:33 +0300

    Update BaseCommand.ts

--- src/BaseCommand.ts
+++ src/BaseCommand.ts
@@ -3,16 +3,20 @@ import crc8 from './crc8';
 import { MideaDeviceType } from './enums/MideaDeviceType';
 
 export default class BaseCommand {
-    data : any[]
+    data: any[]
     device_type: MideaDeviceType
-    
+
     constructor(device_type: MideaDeviceType) {
 
         this.device_type = device_type
 
         if (device_type == MideaDeviceType.AirConditioner) {
-           this.data = [170, 35, 172, 0, 0, 0, 0, 0, 3, 2, 64, 67, 70, 102, 127, 127, 0, 48, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-           
+
+            // More magic numbers. I'm sure each of these have a purpose, but none of it is documented in english. I might make an effort to google translate the SDK
+            // full = [170, 35, 172, 0, 0, 0, 0, 0, 3, 2, 64, 67, 70, 102, 127, 127, 0, 48, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 6, 14, 187, 137, 169, 223, 88, 121, 170, 108, 162, 36, 170, 80, 242, 143, null];
+
+            this.data = [170, 35, 172, 0, 0, 0, 0, 0, 3, 2, 64, 67, 70, 102, 127, 127, 0, 48, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
         } else if (device_type == MideaDeviceType.Dehumidifier) {
             this.data = [
                 // Command Header
@@ -39,7 +43,7 @@ export default class BaseCommand {
                 0, 0, 0, 0, 0, 0, 0, 0, 0
                 // Data End
             ];
-            
+
         } else {
             // Unknown/Unsupported: default to AirCon
             this.data = [170, 35, 172, 0, 0, 0, 0, 0, 3, 2, 64, 67, 70, 102, 127, 127, 0, 48, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
@@ -49,7 +53,7 @@ export default class BaseCommand {
     finalize() {
         // Add the CRC8
         this.data[this.data.length - 1] = crc8.calculate(this.data.slice(16));
-        
+
         // Set the length of the command data
         this.data[0x01] = this.data.length;
 
