713a72adf8094161ad91d4c8bb9f9fd15bb792cb
hillaliy <hillaliy@me.com>
2021-07-20 15:52:47 +0300

    Update MideaAccessory.ts

--- src/MideaAccessory.ts
+++ src/MideaAccessory.ts
@@ -30,7 +30,7 @@ export class MideaAccessory {
 	public name: string = ''
 	public model: string = ''
 	public userId: string = ''
-	public firmwareVersion: string = '1.2.8'
+	public firmwareVersion: string = '1.2.9'
 
 	private service!: Service
 	private fanService!: Service
