47e54c472a58e206f4a8c5064df9b17b59380759
hillaliy <hillaliy@me.com>
2021-07-20 15:52:39 +0300

    Update ACApplianceResponse.ts

--- src/responses/ACApplianceResponse.ts
+++ src/responses/ACApplianceResponse.ts
@@ -10,26 +10,28 @@ export default class ACApplianceResponse extends ApplianceResponse {
     get auxHeat() {
         return (this.data[0x09] & 0x08) > 0;
     }
-
     // Byte 0x0b
     get indoorTemperature() {
         return (this.data[0x0b] - 50) / 2.0;
     }
-
     // Byte 0x0c
     get outdoorTemperature() {
         return (this.data[0x0c] - 50) / 2.0;
     }
+
     get catchCold() {
         // This needs a better name, dunno what it actually means
         return (this.data[0x0a] & 0x08) > 0;
     }
+
     get celsiusUnit() {
         return (this.data[0x09] & 0x80) > 0; // CELSIUS - False
     }
+
     get fahrenheitUnit() {
         return this.data[23] > 0; // FAHRENHEIT - True
     }
+
     get humidity() {
         return this.data[0x0d] & 0x7f;
     }
