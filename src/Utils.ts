868a5bd077a4a7919ce02c1ce3dc31cbbca8dc5a
hillaliy <hillaliy@me.com>
2021-09-05 15:18:20 +0300

    Update Utils.ts

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,6 +1,7 @@
 // Utils – Utility functions
+import { kStringMaxLength } from "buffer";
 import crypto from "crypto";
-import Constants from './Constants'
+import Constants from './Constants';
 
 export default class Utils {
 	static encode(data: number[]): number[] {
@@ -8,36 +9,36 @@ export default class Utils {
 		for (let b of data) {
 			if (b >= 128) {
 				b = b - 256;
-			}
+			};
 			normalized.push(b);
-		}
+		};
 		return normalized;
-	}
+	};
 
 	static decode(data: number[]): number[] {
 		const normalized = [];
 		for (let b of data) {
 			if (b < 0) {
 				b = b + 256;
-			}
+			};
 			normalized.push(b);
-		}
+		};
 		return normalized;
-	}
+	};
 	// Returns a timestamp in the format YYYYMMDDHHmmss
 	static getStamp(): string {
 		const date = new Date();
 		return date.toISOString().slice(0, 19).replace(/-/g, "").replace(/:/g, "").replace(/T/g, "");
-	}
+	};
 
 	static formatResponse(arr: any[]) {
 		let output: string[] = []
 		for (var i = 0; i < arr.length; i++) {
 			let intValue = parseInt(arr[i]);
 			output.push((intValue).toString(2))
-		}
+		};
 		return output;
-	}
+	};
 
 	static getSign(path: string, form: any) {
 		if (path != '' && form) {
@@ -59,8 +60,8 @@ export default class Utils {
 				.digest("hex");
 		} else {
 			return false;
-		}
-	}
+		};
+	};
 
 	static decryptAes(reply: string, dataKey: string) {
 		if (reply && dataKey != '') {
@@ -69,8 +70,8 @@ export default class Utils {
 			return dec.split(",").map(Number);
 		} else {
 			return [];
-		}
-	}
+		};
+	};
 
 	static decryptAesString(reply: string, dataKey: string) {
 		if (reply && dataKey != '') {
@@ -79,8 +80,8 @@ export default class Utils {
 			return dec;
 		} else {
 			return '';
-		}
-	}
+		};
+	};
 
 	static encryptAes(query: number[], dataKey: string) {
 		if (query && dataKey != '') {
@@ -90,8 +91,8 @@ export default class Utils {
 			return ciph;
 		} else {
 			return false;
-		}
-	}
+		};
+	};
 
 	static encryptAesString(query: string, dataKey: string) {
 		if (dataKey != '') {
@@ -101,8 +102,8 @@ export default class Utils {
 			return ciph;
 		} else {
 			return false;
-		}
-	}
+		};
+	};
 
 	static getSignPassword(loginId: string, password: string) {
 		if (loginId != '' && password != '') {
@@ -110,15 +111,16 @@ export default class Utils {
 			return crypto.createHash("sha256").update(loginId + pw + Constants.AppKey).digest("hex");
 		} else {
 			return '';
-		}
-	}
+		};
+	};
+
 	static generateDataKey(accessToken: string) {
 		if (accessToken != '') {
 			const md5AppKey = crypto.createHash("md5").update(Constants.AppKey).digest("hex");
 			const decipher = crypto.createDecipheriv("aes-128-ecb", md5AppKey.slice(0, 16), "");
 			const dec = decipher.update(accessToken, "hex", "utf8");
 			return dec;
-		}
+		};
 		return '';
-	}
-}
+	};
+};
