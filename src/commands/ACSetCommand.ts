52aa6f4e8dfbb2dd4d056c18cb08627314d38a14
hillaliy <hillaliy@me.com>
2021-11-15 23:23:53 +0200

    Update ACSetCommand.ts

--- src/commands/ACSetCommand.ts
+++ src/commands/ACSetCommand.ts
@@ -6,23 +6,23 @@ export default class ACSetCommand extends SetCommand {
 
     constructor(device_type: MideaDeviceType = MideaDeviceType.AirConditioner) {
         super(device_type);
-    }
+    };
     // Byte 0x0c
     get targetTemperature() {
         return this.data[0x0c] & 0x1f;
-    }
+    };
 
     set targetTemperature(temperatureCelsius: number) {
         this.data[0x0c] &= ~0x0f; // Clear the temperature bits
         this.data[0x0c] |= (temperatureCelsius & 0xf) | ((temperatureCelsius << 4) & 0x10);
-    }
+    };
     // Byte 0x14
     get useFahrenheit() {
         return (this.data[0x14] & 0x04) > 0;
-    }
+    };
 
     set useFahrenheit(useFahrenheitEnabled: boolean) {
         // set the unit to fahrenheit from celcius
         this.data[0x14] = useFahrenheitEnabled ? 0x04 : 0;
-    }
-}
+    };
+};
