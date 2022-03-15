cc43b3af0af61667f909a36f374d5bf359a6667d
hillaliy <hillaliy@me.com>
2022-03-15 09:00:54 +0200

    Update DehumidifierApplianceResponse.ts

--- src/responses/DehumidifierApplianceResponse.ts
+++ src/responses/DehumidifierApplianceResponse.ts
@@ -1,16 +1,16 @@
 import ApplianceResponse from '../ApplianceResponse';
 
 export default class DehumidifierApplianceResponse extends ApplianceResponse {
-
-    get currentHumidity() {
-        return this.data[0x10];
-    }
-
+    // Byte 0x07
     get targetHumidity() {
         return this.data[0x07];
     }
-
+    // Byte 0x0a
     get waterLevel() {
         return this.data[0x0a] & 127;
+    }
+    // Byte 0x10
+    get currentHumidity() {
+        return this.data[0x10];
     }
 }
