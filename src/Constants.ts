38a61d9efae87a58dbf89410c23fca4c42dae29a
hillaliy <hillaliy@me.com>
2022-08-14 08:23:52 +0300

    Update Constants.ts

--- src/Constants.ts
+++ src/Constants.ts
@@ -103,29 +103,27 @@ export default class Constants {
 	public static RequestSource: string = '1010' // '17'
 
 	public static SupportedApps: {
-		'NetHomePlus': {
+		NetHomePlus: {
 			AppKey: '3742e9e5842d4ad59c2db887e12449f9',
-			AppId: '1017',
+			AppId: 1017,
 			ApiURL: 'https://mapp.appsmb.com',
 			SignKey: 'xhdiwjnchekd4d512chdjx5d8e4c394D2D7S',
-			Proxied: '',
+			Proxied: null,
 		},
-		'MideaAir': {
+		MideaAir: {
 			AppKey: 'ff0cf6f5f0c3471de36341cab3f7a9af',
-			AppId: '1117',
+			AppId: 1117,
 			ApiURL: 'https://mapp.appsmb.com',
 			SignKey: 'xhdiwjnchekd4d512chdjx5d8e4c394D2D7S',
-			Proxied: '',
+			Proxied: null,
 		},
-		'MSmartHome': {
+		MSmartHome: {
 			AppKey: 'ac21b9f9cbfe4ca5a88562ef25e2b768',
-			AppId: '1010',
+			AppId: 1010,
 			ApiURL: 'https://mp-prod.appsmb.com/mas/v5/app/proxy?alias=',
 			SignKey: 'xhdiwjnchekd4d512chdjx5d8e4c394D2D7S',
-			// meicloud
-			iotKey: 'f4dcd1511147af45775d7e680ac5312b',  // noqa: E501
-			// PROD_VnoClJI9aikS8dyy
-			hmacKey: '5018e65c32bcec087e6c01631d8cf55398308fc19344d3e130734da81ac2e162',
+			iotKey: 'meicloud',
+			hmacKey: 'PROD_VnoClJI9aikS8dyy',
 			Proxied: 'v5',
 		},
 	}
