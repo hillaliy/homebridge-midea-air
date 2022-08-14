b4e4146c94ecfe22d6e74d515314c41ab930e644
hillaliy <hillaliy@me.com>
2022-08-14 07:20:10 +0300

    Update MideaErrorCodes.ts

--- src/enums/MideaErrorCodes.ts
+++ src/enums/MideaErrorCodes.ts
@@ -1,10 +1,17 @@
 // MideaErrorCodes enum – This is a list of errors and their meaning to better understand them in code
 export enum MideaErrorCodes {
+	// MideaAir / NetHomePlus
 	DeviceUnreachable = 3123,
 	CommandNotAccepted = 3176,
 	InvalidLogin = 3101,
 	InvalidSession = 3106,
 	SignIllegal = 3301,
 	ValueIllegal = 3004,
-	NoOpenIdOrUnionId = 3002
+	NoOpenIdOrUnionId = 3002,
+	// MSmartHome
+	UnknownError = 1,
+	InvalidArgument = 30005,
+	NoAccessTokenSupplied = 40001,
+	NoRouteMatchedWithThoseValues = 40404,
+	BadSignature = 44003,
 }
