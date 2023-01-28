727a39c0fba0afcbb7f75b76348333b59a761e02
hillaliy <hillaliy@me.com>
2023-01-28 19:32:52 +0200

    Update DehumidifierApplianceResponse.ts

--- src/responses/DehumidifierApplianceResponse.ts
+++ src/responses/DehumidifierApplianceResponse.ts
@@ -1,4 +1,4 @@
-import ApplianceResponse from '../ApplianceResponse';
+import ApplianceResponse from './ApplianceResponse';
 
 export default class DehumidifierApplianceResponse extends ApplianceResponse {
     // Byte 0x02
